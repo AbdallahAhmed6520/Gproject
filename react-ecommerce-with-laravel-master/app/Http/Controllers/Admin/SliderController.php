860ffc05d2a8167dc4438776d2a099453146d085
Abdallah Ahmed <58490181+abdosedroho@users.noreply.github.com>
2022-07-01 22:43:15 +0200

    fix

--- react-ecommerce-with-laravel-master/app/Http/Controllers/Admin/SliderController.php
+++ react-ecommerce-with-laravel-master/app/Http/Controllers/Admin/SliderController.php
@@ -9,39 +9,43 @@ use Image;
 
 class SliderController extends Controller
 {
-    public function AllSlider(){
+    public function AllSlider()
+    {
         $result = HomeSlider::all();
         return $result;
-    } // End Mehtod 
+    } // End Mehtod
 
 
-    public function GetAllSlider(){
+    public function GetAllSlider()
+    {
         $slider = HomeSlider::latest()->get();
-        return view('backend.slider.slider_view',compact('slider'));
-    } // End Mehtod 
+        return view('backend.slider.slider_view', compact('slider'));
+    } // End Mehtod
 
 
-    public function AddSlider(){
+    public function AddSlider()
+    {
 
-         return view('backend.slider.slider_add');
+        return view('backend.slider.slider_add');
 
-    }// End Mehtod 
+    }// End Mehtod
 
-    public function StoreSlider(Request $request){
+    public function StoreSlider(Request $request)
+    {
 
-         $request->validate([
+        $request->validate([
             'slider_image' => 'required',
-        ],[
+        ], [
             'slider_image.required' => 'Upload Slider Image'
 
         ]);
 
         $image = $request->file('slider_image');
-        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
-        Image::make($image)->resize(1024,379)->save('upload/slider/'.$name_gen);
-        $save_url = 'http://127.0.0.1:8000/upload/slider/'.$name_gen;
+        $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+        Image::make($image)->resize(1024, 379)->save('upload/slider/' . $name_gen);
+        $save_url = 'http://127.0.0.1:8000/upload/slider/' . $name_gen;
 
-        HomeSlider::insert([          
+        HomeSlider::insert([
             'slider_image' => $save_url,
         ]);
 
@@ -52,26 +56,28 @@ class SliderController extends Controller
 
         return redirect()->route('all.slider')->with($notification);
 
-    }// End Mehtod 
+    }// End Mehtod
 
 
-    public function EditSlider($id){
+    public function EditSlider($id)
+    {
         $slider = HomeSlider::findOrFail($id);
-        return view('backend.slider.slider_edit',compact('slider'));
+        return view('backend.slider.slider_edit', compact('slider'));
 
-    } // End Mehtod 
+    } // End Mehtod
 
 
-    public function UpdateSlider(Request $request){
+    public function UpdateSlider(Request $request)
+    {
 
         $slider_id = $request->id;
 
         $image = $request->file('slider_image');
-        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
-        Image::make($image)->resize(1024,379)->save('upload/slider/'.$name_gen);
-        $save_url = 'http://127.0.0.1:8000/upload/slider/'.$name_gen;
+        $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+        Image::make($image)->resize(1024, 379)->save('upload/slider/' . $name_gen);
+        $save_url = 'http://127.0.0.1:8000/upload/slider/' . $name_gen;
 
-        HomeSlider::findOrFail($slider_id)->update([          
+        HomeSlider::findOrFail($slider_id)->update([
             'slider_image' => $save_url,
         ]);
 
@@ -82,10 +88,11 @@ class SliderController extends Controller
 
         return redirect()->route('all.slider')->with($notification);
 
-    } // End Mehtod 
+    } // End Mehtod
 
 
-    public function DeleteSlider($id){
+    public function DeleteSlider($id)
+    {
 
         HomeSlider::findOrFail($id)->delete();
 
@@ -96,7 +103,6 @@ class SliderController extends Controller
 
         return redirect()->back()->with($notification);
 
-    } // End Mehtod 
+    } // End Mehtod
 
 }
-  
